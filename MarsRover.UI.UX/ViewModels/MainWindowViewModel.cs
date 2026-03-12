0633fb34a43890c7781b204977a3674c3264f882
Katona Gergő <geri.katona.mechwartsuli23@gmail.com>
2026-03-12 08:09:48 +0100

    Removing comments from the UI

--- MarsRover.UI.UX/ViewModels/MainWindowViewModel.cs
+++ MarsRover.UI.UX/ViewModels/MainWindowViewModel.cs
@@ -6,14 +6,10 @@ namespace MarsRover.ViewModels
     public partial class MainWindowViewModel : ViewModelBase
     {
         public ObservableCollection<MapCellViewModel> MapCells { get; } = new();
-
-        // --- EZT A RÉSZT ADDD HOZZÁ ---
         public MainWindowViewModel()
         {
-            // Legyártunk 2500 cellát (50x50-es rács)
             for (int i = 0; i < 2500; i++)
             {
-                // Alapértelmezett szürke szín minden cellának
                 var cell = new MapCellViewModel { Color = Brushes.DimGray };
 
                 MapCells.Add(cell);
--- MarsRover.UI.UX/ViewModels/MapCellViewModel.cs
+++ MarsRover.UI.UX/ViewModels/MapCellViewModel.cs
@@ -3,8 +3,6 @@ using CommunityToolkit.Mvvm.ComponentModel;
 
 namespace MarsRover.ViewModels;
 
-// Figyelj rá, hogy ne legyen másik "class MapCellViewModel" körötte!
-// Az osztálynak "partial"-nak kell lennie és a ViewModelBase-ből kell származnia.
 public partial class MapCellViewModel : ViewModelBase
 {
     [ObservableProperty]
