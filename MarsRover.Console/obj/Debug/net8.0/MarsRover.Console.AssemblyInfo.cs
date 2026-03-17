945938c6b2ab6fa04f6f544502f45fd721211563
Katona Gergő <geri.katona.mechwartsuli23@gmail.com>
2026-03-17 19:49:57 +0100

    Removed curved lines from MenuScreen

--- MarsRover.Console/obj/Debug/net8.0/MarsRover.Console.AssemblyInfo.cs
+++ MarsRover.Console/obj/Debug/net8.0/MarsRover.Console.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MarsRover.Console")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+45ea2ca1ba12257910b46ff425c125c2a3856d67")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d0ad83f1a27aab5e4f08a7eb0ac8d63fa88e97b8")]
 [assembly: System.Reflection.AssemblyProductAttribute("MarsRover.Console")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MarsRover.Console")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
